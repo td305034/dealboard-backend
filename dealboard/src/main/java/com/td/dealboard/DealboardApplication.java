681112aa8dabe330e4eac6f2926d53c261abc038
td305034 <elektroman35@gmail.com>
2025-10-09 19:49:08 +0200

    initial commit

---
+++ dealboard/src/main/java/com/td/dealboard/DealboardApplication.java
@@ -0,0 +1,13 @@
+package com.td.dealboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealboardApplication.class, args);
+	}
+
+}
---
+++ dealboard/src/test/java/com/td/dealboard/DealboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.td.dealboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
