9b0a5d34546341dcbc293626c4132b05bce1b02e
td305034 <elektroman35@gmail.com>
2025-10-09 23:45:19 +0200

    add user repo

--- dealboard/src/main/java/com/td/dealboard/user/UserRepository.java
+++ dealboard/src/main/java/com/td/dealboard/user/UserRepository.java
@@ -1,4 +1,9 @@
 package com.td.dealboard.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }
