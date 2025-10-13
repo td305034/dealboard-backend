e72dcfa49f38c4b83062bf0da9d01f5ded536156
td305034 <elektroman35@gmail.com>
2025-10-13 22:19:34 +0200

    feat: finish basic security
    Created security configuration class, added auth controller, auth service for registering and authenticating, also added config class for OpenApi.

---
+++ dealboard/src/main/java/com/td/dealboard/auth/AuthenticationService.java
@@ -0,0 +1,50 @@
+package com.td.dealboard.auth;
+
+import com.td.dealboard.config.JwtService;
+import com.td.dealboard.user.Role;
+import com.td.dealboard.user.User;
+import com.td.dealboard.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request){
+        User user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token((jwtToken))
+                .build();
+    }
+    public AuthenticationResponse authenticate(AuthenticationRequest request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token((jwtToken))
+                .build();
+    }
+}
