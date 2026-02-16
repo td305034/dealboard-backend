7bc2a4117c8cd2c0712ac9bf64582db2f3f761e2
td305034 <elektroman35@gmail.com>
2026-02-16 12:48:12 +0100

    feat: searching users query for admin panel

--- dealboard/src/main/java/com/td/dealboard/user/UserRepository.java
+++ dealboard/src/main/java/com/td/dealboard/user/UserRepository.java
@@ -1,6 +1,10 @@
 package com.td.dealboard.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -9,4 +13,10 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByEmail(String email);
 
     Optional<User> findByRefreshToken(String refreshTokenCookie);
+
+    @Query("SELECT u FROM User u WHERE " +
+            "LOWER(CAST(u.id AS string)) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(u.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(u.email) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    Page<User> findByKeyword(String keyword, Pageable pageable);
 }
